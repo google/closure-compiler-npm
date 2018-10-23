34256b05da187bfc9d73740885fd82bca02749bc
Chad Killingsworth <chadkillingsworth@gmail.com>
2018-10-23 18:14:24 -0500

    Add more comments and cleanup the build script to make the intent clearer

--- build-scripts/build_compiler.js
+++ build-scripts/build_compiler.js
@@ -21,6 +21,13 @@
  * Invoked as part of the package build process:
  *
  *     yarn run build
+ *
+ * For pull requests and pushes to master, the compiler submodule is expected to be pointed at the tagged commit
+ * that matches the package major version.
+ *
+ * When the COMPILER_NIGHTLY env variable is set, the compiler submodule will be pointing to the latest master
+ * commit. This is for regular integration testing of the compiler with the various tests in this repo's packages.
+ * In this case the compiler will be built as a SNAPSHOT.
  */
 'use strict';
 
@@ -36,31 +43,36 @@ const packageVersion = new Semver(packageInfo.version);
 const fs = require('fs');
 const glob = require('glob');
 
-const gitTagResults = spawnSync('git', ['tag', '--points-at', 'HEAD'], {
-  cwd: './compiler'
-});
-let compilerVersion = '1.0-SNAPSHOT';
-if (gitTagResults.status === 0) {
-  const currentTag = gitTagResults.stdout.toString().replace(/\s/g, '');
-  let normalizedTag = currentTag;
-  if (normalizedTag) {
-    // Standard release tags are of the form "vYYYYMMDD".
-    // We also recognize the form where a special release name proceeds it:
-    //     "webpack-v20180810"
-    //     "v20180810-gwt-fix"
-    normalizedTag = currentTag.replace(/^([a-z]+-)?(v\d{8})(.*)$/, '$2');
+function getCompilerSubmoduleReleaseVersion() {
+  const gitTagResults = spawnSync('git', ['tag', '--points-at', 'HEAD'], {
+    cwd: './compiler'
+  });
+  if (gitTagResults.status === 0) {
+    const currentTag = gitTagResults.stdout.toString().replace(/\s/g, '');
+    let normalizedTag = currentTag;
+    if (normalizedTag) {
+      // Standard release tags are of the form "vYYYYMMDD".
+      // We also recognize the forms where a special release name is present:
+      //     "webpack-v20180810"
+      //     "v20180810-gwt-fix"
+      normalizedTag = currentTag.replace(/^([a-z]+-)?(v\d{8})(.*)$/, '$2');
+    }
+    return normalizedTag;
   }
+}
 
-  // If the compiler submodule is pointing at a tagged version that matches the package
-  // major version, update the compiler build to use the correct version number.
-  if (normalizedTag === `v${packageVersion.major}`) {
-    compilerVersion = normalizedTag;
-    const pomPaths = glob.sync('./compiler/**/pom*.xml');
-    pomPaths.forEach(pomPath => {
-      const contents = fs.readFileSync(pomPath, 'utf8');
-      fs.writeFileSync(pomPath, contents.replace('<version>1.0-SNAPSHOT</version>', `<version>${compilerVersion}</version>`), 'utf8');
-    });
-  }
+let compilerVersion = getCompilerSubmoduleReleaseVersion();
+if (compilerVersion === `v${packageVersion.major}`) {
+  // Since the tagged commit pom.xml files are still set as SNAPSHOT versions,
+  // update these files to the correct version so that the `compiler --version` command is correct.
+  // TODO(chadkillingsworth): Find a better way to do this
+  const pomPaths = glob.sync('./compiler/**/pom*.xml');
+  pomPaths.forEach(pomPath => {
+    const contents = fs.readFileSync(pomPath, 'utf8');
+    fs.writeFileSync(pomPath, contents.replace('<version>1.0-SNAPSHOT</version>', `<version>${compilerVersion}</version>`), 'utf8');
+  });
+} else {
+  compilerVersion = '1.0-SNAPSHOT';
 }
 
 const compilerJavaBinaryPath = `./compiler/target/closure-compiler-${compilerVersion}.jar`;
@@ -69,31 +81,16 @@ const compilerJsBinaryPath = `./compiler/target/closure-compiler-gwt-${compilerV
 console.log(process.platform, process.arch, compilerVersion);
 
 function copyCompilerBinaries() {
-  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-java/compiler.jar', err => {
+  const reportErrors = err => {
     if (err) {
       throw new Error(err);
     }
-  });
-  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-linux/compiler.jar', err => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-osx/compiler.jar', err => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-  ncp(compilerJsBinaryPath, './packages/google-closure-compiler-js/jscomp.js', err => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-  ncp('./compiler/contrib', './packages/google-closure-compiler/contrib', err => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
+  };
+  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-java/compiler.jar', reportErrors);
+  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-linux/compiler.jar', reportErrors);
+  ncp(compilerJavaBinaryPath, './packages/google-closure-compiler-osx/compiler.jar', reportErrors);
+  ncp(compilerJsBinaryPath, './packages/google-closure-compiler-js/jscomp.js', reportErrors);
+  ncp('./compiler/contrib', './packages/google-closure-compiler/contrib', reportErrors);
 }
 
 if (!fs.existsSync(compilerJavaBinaryPath) || !fs.existsSync(compilerJsBinaryPath)) {
@@ -118,6 +115,9 @@ if (!fs.existsSync(compilerJavaBinaryPath) || !fs.existsSync(compilerJsBinaryPat
       process.exit(1);
       return;
     }
+    // Add a license header to the gwt version jscomp.js file since the compiler build omits this.
+    // If the gwt version ever has a source map, the source mappings will need updated to account for the
+    // prepended lines.
     fs.writeFileSync(
         compilerJsBinaryPath,
         `/*

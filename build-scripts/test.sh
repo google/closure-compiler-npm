2153d69af0bb8d911e8feb0b6777bed4c4b38270
Chad Killingsworth <chadkillingsworth@gmail.com>
2018-11-09 07:17:51 -0600

    Fail the build scripts if any single command fails

--- build-scripts/test.sh
+++ build-scripts/test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-yarn workspaces run test "$@"
-mocha "$@"
+# Run the test commands and fail the script if any of them failed
+EXIT_STATUS=0
+yarn workspaces run test "$@" || EXIT_STATUS=$?
+mocha "$@" || EXIT_STATUS=$?
+exit $EXIT_STATUS

f5fb6ceef0a34311ba77b09b872b9451b0f00136
Chad Killingsworth <chadkillingsworth@gmail.com>
2019-05-31 06:41:03 -0500

    Initial work to support windows native images

--- build-scripts/graal-env.js
+++ build-scripts/graal-env.js
@@ -15,7 +15,8 @@
  */
 'use strict';
 
-const GRAAL_OS = process.platform === 'darwin' ? 'macos' : process.platform;
+const GRAAL_OS = process.platform === 'darwin' ? 'macos' :
+    (process.platform === 'win32' ? 'windows' : process.platform);
 const GRAAL_VERSION = process.env.GRAAL_VERSION || '19.0.0';
 const GRAAL_FOLDER = `graalvm-ce-${GRAAL_OS}-amd64-${GRAAL_VERSION}`;
 const GRAAL_URL = process.env.GRAAL_URL ||

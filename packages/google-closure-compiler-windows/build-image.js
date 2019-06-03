51fbbf84511f931af7368ebefe900ad893bb1711
Chad Killingsworth <chad.killingsworth@banno.com>
2019-06-03 14:37:58 -0500

    Graal compiling on windows

--- packages/google-closure-compiler-windows/build-image.js
+++ packages/google-closure-compiler-windows/build-image.js
@@ -28,13 +28,34 @@ const runCommand = require('../../build-scripts/run-command');
 
 if (fs.existsSync(path.resolve(__dirname, 'compiler'))) {
   process.stdout.write(`  ${DIM}google-closure-compiler-windows binary already exists${RESET}\n`);
+  process.exit(0);
 } else if (process.platform !== 'win32') {
   process.stdout.write(`  ${DIM}google-closure-compiler-windows build wrong platform${RESET}\n`);
-} else {
-  process.stdout.write(`  ${DIM}google-closure-compiler-windows building image${RESET}\n`);
-  runCommand('node ../../build-scripts/graal.js',{stdio: 'inherit'})
-      .catch(e => {
-        console.error(e);
-        process.exit(1);
-      });
+  process.exit(0);
 }
+process.stdout.write(`  ${DIM}google-closure-compiler-windows building image${RESET}\\n`);
+
+const NET_FRAMEWORK_VERSION = fs.readdirSync('C:\\WINDOWS\\Microsoft.NET\\Framework64', 'utf8').find(filepath => /^v4/.test(filepath));
+process.env.APPVER = '6.1';
+process.env.CL = `/AI C:\\WINDOWS\\Microsoft.NET\\Framework64\\${NET_FRAMEWORK_VERSION}`;
+process.env.CommandPromptType = 'Native';
+process.env.Configuration = 'Debug';
+process.env.CURRENT_CPU = 'x64';
+process.env.FrameworkVersion = NET_FRAMEWORK_VERSION;
+process.env.INCLUDE = 'C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\INCLUDE;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\INCLUDE;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\INCLUDE\\gl;';
+process.env.LIB = 'C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\Lib\\amd64;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Lib\\X64;';
+process.env.LIBPATH =`C:\\WINDOWS\\Microsoft.NET\\Framework64\\${NET_FRAMEWORK_VERSION};C:\\WINDOWS\\Microsoft.NET\\Framework\\${NET_FRAMEWORK_VERSION};C:\\WINDOWS\\Microsoft.NET\\Framework64\\v3.5;C:\\WINDOWS\\Microsoft.NET\\Framework\\v3.5;C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\Lib\\amd64;`;
+process.env.Path = `C:\\WINDOWS\\Microsoft.NET\\Framework64\\${NET_FRAMEWORK_VERSION};C:\\WINDOWS\\Microsoft.NET\\Framework\\${NET_FRAMEWORK_VERSION};C:\\WINDOWS\\Microsoft.NET\\Framework64\\v3.5;C:\\WINDOWS\\Microsoft.NET\\Framework\\v3.5;C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\Common7\\IDE;C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\Common7\\Tools;C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\Bin\\amd64;C:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\Bin\\VCPackages;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Bin\\NETFX 4.0 Tools\\x64;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Bin\\x64;C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Bin;` + process.env.PATH;
+process.env.PlatformToolset = 'Windows7.1SDK';
+process.env.sdkdir = 'C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\';
+process.env.TARGET_CPU = 'x86';
+process.env.TARGET_PLATFORM = 'WIN7';
+process.env.ToolsVersion = '4.0';
+process.env.WindowsSDKDir = 'C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\';
+process.env.WindowsSDKVersionOverride = 'v7.1';
+
+runCommand('node ../../build-scripts/graal.js',{stdio: 'inherit'})
+    .catch(e => {
+      console.error(e);
+      process.exit(1);
+    });

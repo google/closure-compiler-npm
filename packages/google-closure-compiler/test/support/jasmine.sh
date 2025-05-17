69fc904dc1c71bd2906856ab779a364c6bde80c4
Chad Killingsworth <chadkillingsworth@gmail.com>
2025-05-17 10:23:46 -0500

    Use a bash script to run the jasmine launcher
    Required for windows builds

--- packages/google-closure-compiler/test/support/jasmine.sh
+++ packages/google-closure-compiler/test/support/jasmine.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Run the test commands and fail the script if any of them failed
 EXIT_STATUS=0
-./packages/google-closure-compiler/test/support/jasmine-launcher.js "$@" || EXIT_STATUS=$?
+./test/support/jasmine-launcher.js "$@" || EXIT_STATUS=$?
 exit $EXIT_STATUS
